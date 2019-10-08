766f35945b18a7f4029381080d16c5ee60269c24
Jose Oña <jhosua57@gmail.com>
2019-10-07 22:41:18 -0400

    Animacion en las imagenes

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,11 @@
+export interface InfoPagina{
+    email?: string;
+    titulo?: string;
+    nombre_corto?: string;
+    pagina_autor?: string;
+    facebook?: string;
+    twitter?: string;
+    instagram?: string;
+    equipo_trabajo?: any[]
+}
+
